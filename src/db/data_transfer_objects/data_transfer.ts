17073154149d6bf0d8e9b3ce645e05a02ca0e108
Bodi <bodiszabo@berkeley.edu>
2025-08-22 10:52:16 -0700

    testing coderabbit

--- src/db/data_transfer_objects/data_transfer.ts
+++ src/db/data_transfer_objects/data_transfer.ts
@@ -4,6 +4,7 @@ import { macros } from "../schema";
 function createMacrosDTO(food: macros): Macros {
     return {
         name: food.name,
+        testing: food.testing,
         brand: food.brand,
         serving_size: food.serving_size,
         macro_percentage_error_estimate: food.macro_percentage_error_estimate,
